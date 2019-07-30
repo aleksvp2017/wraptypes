3c75210ed8fe37f46092e75243d0952febe44c9a
Aleksander Pascoal <aleksander.pascoal@planejamento.gov.br>
2019-07-30 10:18:28 -0300

    Includes wrap functions for string, int32 and bool

---
+++ pkg/wrap.go
@@ -0,0 +1,19 @@
+package wraptypes
+
+//WrapBool retorna um ponteiro a partir do primitivo
+func WrapBool(b bool) *bool {
+	wrap := b
+	return &wrap
+}
+
+//WrapInt32 retorna um ponteiro a partir do primitivo
+func WrapInt32(i int32) *int32 {
+	wrap := i
+	return &wrap
+}
+
+//WrapString retorna um ponteiro a partir do primitivo
+func WrapString(s string) *string {
+	wrap := s
+	return &wrap
+}
